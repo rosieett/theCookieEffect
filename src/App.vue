91fbbbf326714f8a5dfcd4704727288d0e9d53de
rosieett <rosie.ett@gmail.com>
2022-02-16 21:45:16 -0500

    layout changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <header><router-link to="/">Home</router-link> - <router-link to="/about">About</router-link></header>
+    <!-- <header><router-link to="/">Home</router-link> - <router-link to="/about">About</router-link></header> -->
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,135 @@
 <template>
 <div class="home">
-    <!-- <hello-world />
-     <div id="form">
-        <div v-for="(category, index) in categories">
-            <h3>{{ category.name }}</h3>
-            <h4>{{ category.description }}</h4>
-            <div v-for="option in category.options">
-                <input type="radio" :name="category.id" id="one" v-bind:value="option.value" v-model="results[index]">
-                <label for="one">{{ option.label }}</label>
-            </div>
-        </div>
-    </div>
 
-    {{ results }} {{ biteResult }} -->
+    <b-container class="bv-example-row header">
+        <b-row>
+            <h1>The Cookie Effect</h1>
+        </b-row>
+        <b-row>
+            <h5>Select your preferences to find your perfect cookie recipe</h5>
+        </b-row>
+    </b-container>
 
     <div id="form">
 
-        <div class="headers" v-for="(category, index) in categories">
-            <div class="section">
-                <h3>{{ category.name }}</h3>
-                <h4>{{ category.description }}</h4>
+        <b-container class="bv-example-row">
+            <b-row>
+                <b-col>
+                    <div class="cardRow" v-for="(category, index) in categories">
 
-                <b-container class="bv-example-row cardRow">
-                    <b-row>
-                        <b-col class="cardBtn" v-for="option in category.options">
+                        <div class="panel">
+                            <div class="text">
+                            <h3>{{ category.name }}</h3>
+                            <h4>{{ category.description }}</h4>
+                            </div>
+                            <div class="cardBtn" v-for="option in category.options">
 
-                            <input type="radio" :name="category.id" id="one" v-bind:value="option.value" v-model="results[index]">
-                            <label for="one">{{ option.label }} </label>
-                        </b-col>
-                    </b-row>
-                </b-container>
-            </div>
-        </div>
-    </div>
+                                <input type="radio" :name="category.id" id="one" v-bind:value="option.value" v-model="results[index]">
+                                <label for="one">{{ option.label }} </label>
+                            </div>
+                        </div>
+                    </div>
+                </b-col>
+                <b-col class="recipe">
+                    <h3>Recipe Area</h3>
 
     {{ spreadResult }} {{ biteResult }}
+                </b-col>
+
+            </b-row>
+
+        </b-container>
+    </div>
+
+    <!-- <div class="recipe">
+
+</div> -->
+
 
 </div>
-
 </template>
+
+<style scoped>
+.header {
+    text-align: center;
+    padding: 20px;
+    margin-top: 30px;
+}
+
+.recipe {
+    background-color: rgb(153, 214, 214);
+}
+
+h3 {
+    font-size: 20px;
+}
+
+.recipe h3{
+    padding-top: 30px;
+}
+
+h4 {
+    font-size: 14px;
+}
+
+label {
+    font-size: 16px;
+}
+
+.text{
+    margin: 5px;
+}
+
+#form {
+    display: flex;
+    flex-direction: column;
+    padding: 30px;
+    background-color: #eeeeee;
+}
+
+.panel {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    position: relative;
+    margin: 10px;
+    /* border: solid 2px #dddddd; */
+    padding: 20px;
+    background-color: white;
+}
+
+input[type="radio"] {
+    /* display: none; */
+}
+
+.cardRow .cardBtn label {
+    display: block;
+    border: 2px solid #bbbbbb;
+    text-align: center;
+    padding: 15px;
+}
+
+.cardRow .cardBtn .title {
+    font-size: 18px;
+    color: black;
+    padding: 40px;
+    font-weight: 600;
+}
+
+.cardRow .cardBtn label:hover {
+    border: 2px solid #bbbbbb;
+    background: #8eacd4;
+}
+
+.cardRow .cardBtn input:checked+label {
+    background: #8eacd4;
+    border: 2px solid #777777;
+}
+
+.cardRow .cardBtn input:checked+label .title {
+    color: #eee;
+}
+</style>
 
 <script>
 // @ is an alias to /src
@@ -47,14 +138,15 @@ export default {
     name: 'Home',
     data() {
         return {
-            categories: [{
+            categories: [
+                {
                     name: 'Spread',
                     id: 'spread',
                     description: 'The way a cookie expands and settles while baking',
                     options: [{
                             label: "Wide",
                             value: "wide",
-                            imgUrl:"https://unsplash.com/photos/DqvMdSbWlBE"
+                            imgUrl: "https://unsplash.com/photos/DqvMdSbWlBE"
                         },
                         {
                             label: "Compact",
@@ -64,9 +156,9 @@ export default {
                     ],
                 },
                 {
-                    name: 'Bite',
-                    id: 'bite',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                    name: 'Tenderness',
+                    id: 'tenderness',
+                    description: 'Heirloom biodiesel quinoa mlkshk asymmetrical master cleanse affogato blog marfa chillwave semiotics gastropub',
                     imgUrl: "",
                     options: [{
                             label: "Crunchy",
@@ -80,17 +172,90 @@ export default {
                         },
                     ],
                 },
+                {
+                    name: 'Rise',
+                    id: 'rise',
+                    description: 'Offal waistcoat occupy humblebrag pinterest, pour-over food truck bitters.',
+                    imgUrl: "",
+                    options: [{
+                            label: "Mound",
+                            value: "mound",
+                            imgUrl: ""
+                        },
+                        {
+                            label: "Thin",
+                            value: "thin",
+                            imgUrl: ""
+                        },
+                    ],
+                },
+                {
+                    name: 'Texture',
+                    id: 'texture',
+                    description: 'Banjo flannel pour-over fixie twee humblebrag.',
+                    imgUrl: "",
+                    options: [{
+                            label: "Fudgy",
+                            value: "fudgy",
+                            imgUrl: ""
+                        },
+                        {
+                            label: "Cakey",
+                            value: "cakey",
+                            imgUrl: ""
+                        },
+                    ],
+                },
+                {
+                    name: 'Chocolate',
+                    id: 'chocolate',
+                    description: 'Mixtape edison bulb tattooed blog, brunch seitan live-edge plaid forage',
+                    imgUrl: "",
+                    options: [{
+                            label: "Small Chunks",
+                            value: "small",
+                            imgUrl: ""
+                        },
+                        {
+                            label: "Large Chunks",
+                            value: "large",
+                            imgUrl: ""
+                        },
+                        {
+                            label: "Swirled In",
+                            value: "swirled",
+                            imgUrl: ""
+                        },
+                    ],
+                },
+                {
+                    name: 'Flavor',
+                    id: 'flavor',
+                    description: 'Sartorial single-origin coffee VHS green juice salvia.',
+                    imgUrl: "",
+                    options: [{
+                            label: "Dark, toffee",
+                            value: "dark",
+                            imgUrl: ""
+                        },
+                        {
+                            label: "Light, milky",
+                            value: "light",
+                            imgUrl: ""
+                        },
+                    ],
+                },
             ],
             results: []
         }
     },
     computed: {
         spreadResult() {
-            return this.results[0]
+            return this.results
         },
-        biteResult() {
-            return this.results[1]
-        }
+        // biteResult() {
+        //     return this.results[1]
+        // }
     },
     methods: {
         onDeleteLastItem() {
@@ -102,71 +267,3 @@ export default {
     },
 };
 </script>
-
-<style scoped>
-.home {
-    background-color: lightgoldenrodyellow;
-    padding: 20px;
-}
-
-.home .hello {
-    background-color: darkgray;
-}
-
-
-#form {
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 60%;
-    padding: 30px;
-    /* background-color: #cccccc; */
-}
-
-.section {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    margin: 30px;
-    border: solid 2px #dddddd;
-    padding: 20px;
-}
-
-
-.cardRow .cardBtn input{
-    /* display: none; */
-}
-
-.cardRow .cardBtn label{
-    display: block;
-    border: 3px solid #b3cbeb;
-    text-align: center;
-    padding: 35px;
-}
-
-.cardRow .cardBtn .title{
-    font-size: 18px;
-    color: black;
-    padding: 40px;
-    font-weight: 600;
-}
-
-.cardRow .cardBtn label:hover{
-    border: 2px solid gray;
-    background: #ccc;
-}
-
-.cardRow .cardBtn input:checked + label {
-    background: #8eacd4;
-}
-
-
-.cardRow .cardBtn input:checked + label .title {
-    color: #eee;
-}
-
-</style>
